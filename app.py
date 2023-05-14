46955048d11d872b731521f7a03dd05758c35b2c
j-c-anderson <j.c.ande13@gmail.com>
2023-05-14 13:01:48 -0500

    Test gsheet

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-from gsheetsdb import connect
 
 # Read data from Google Sheet. URL saved in Secrets with Streamlit
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
